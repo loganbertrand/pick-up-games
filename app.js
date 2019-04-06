435e64d3102aeb44008f644bb38e22b67d84ae39
Logan Bertrand <loganwbertrand@gmail.com>
2019-04-06 14:02:49 -0700

    comments in JS

---
+++ app.js
@@ -0,0 +1,17 @@
+// SD Pick Up Games
+
+//Sign in to have user data and 
+
+// Search for Pick up games that have been posted around you
+
+    //Show the amount of people going, the location, type of game and a description that the poster has created
+
+    //Click that you are interested in going
+
+// Post a Pick Up Game for people to join
+
+    //Enter the category for the type of sport
+
+    //Short Description on what exactly you want to do, where in the park you might be
+
+    //Choose the park that you are going to, from a list of parks nearby
